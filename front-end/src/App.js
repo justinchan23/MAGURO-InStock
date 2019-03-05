49afe882c0e517ef9a9ea39b2c9b32af7131590d
justinchan23 <justinchan23@gmail.com>
2019-03-05 13:47:24 -0800

    updated routes for warehouses and inventory

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import Nav from './Components/Nav'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Main from './Components/Main'
+import WarehouseLocations from './Components/WarehouseLocations'
+import Inventory from './Components/Inventory'
 
 const App = () => (
   <div>
@@ -11,8 +13,8 @@ const App = () => (
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route path="/warehouses" component={Main} />
-          <Route path="/inventory" component={Main} />
+          <Route path="/warehouses" component={WarehouseLocations} />
+          <Route path="/inventory" component={Inventory} />
           <Route path="/warehouses/:id" component={Main} />
           <Route path="/inventory/:id" component={Main} />
         </Switch>
