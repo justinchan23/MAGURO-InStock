800d04986c04aa2308d9b8f849887d7afc212f6f
Connor Straathof <cstraathof@gmail.com>
2019-03-05 14:08:11 -0800

    nav first edit

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -1,3 +1,22 @@
-import React from 'react'
+import React, { Component } from "react";
+import "../src/App.scss";
+// import { Link } from "react-router-dom"
+import Logo from '../src/Logo-instock.svg'
 
-const Nav = () => {}
+class App extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <ul>
+          <div className="nav-components">
+            <li className="nav-logo"><img src={Logo} alt="some value"/></li>
+            <li className="nav-inventory">Inventory</li>
+            <li className="nav-location">Locations</li>
+          </div>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default App;
