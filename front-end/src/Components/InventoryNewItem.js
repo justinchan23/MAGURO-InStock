b0e3ad673a03c25f69eb74843f43bfff9a0de8a2
justinchan23 <justinchan23@gmail.com>
2019-03-05 15:32:39 -0800

    fix syntax to eliminate console errors

--- front-end/src/Components/InventoryNewItem.js
+++ front-end/src/Components/InventoryNewItem.js
@@ -3,42 +3,44 @@ import '../Styles/InventoryNewItem/InventoryNew.css'
 
 const InventoryNewItem = () => {
   return (
-    <content class="warehouseNew">
-      <div class="warehouseNew__main">
-        <div class="warehouseNew__section">
+    <content className="warehouseNew">
+      <div className="warehouseNew__main">
+        <div className="warehouseNew__section">
           <h1>Create New</h1>
-          <form class="warehouseNew__form">
-            <label for="product">
-              PRODUCT <input class="warehouseNew__name" type="text" placeholder="Item Name" />
+          <form className="warehouseNew__form">
+            <label>
+              PRODUCT <input className="warehouseNew__name" type="text" placeholder="Item Name" />
             </label>
-            <label for="lastOrdered">
-              LAST ORDERED <input class="warehouseNew__name" type="text" placeholder="yyyy-mm-dd" />
+            <label>
+              LAST ORDERED{' '}
+              <input className="warehouseNew__name" type="text" placeholder="yyyy-mm-dd" />
             </label>
-            <label for="city">
-              CITY <input class="warehouseNew__name" type="text" placeholder="City" />
+            <label>
+              CITY <input className="warehouseNew__name" type="text" placeholder="City" />
             </label>
-            <label for="country">
+            <label>
               COUNTRY
-              <select name="country" class="warehouseNew__select">
+              <select name="country" className="warehouseNew__select">
                 <option value="Canada">Canada</option>
                 <option value="US">US</option>
                 <option value="Mexico">Mexico</option>
               </select>
             </label>
-            <label for="quantity">
-              QUANTITY <input class="warehouseNew__name" type="text" placeholder="0" />
+            <label>
+              QUANTITY <input className="warehouseNew__name" type="text" placeholder="0" />
             </label>
-            <label for="status">
-              STATUS <input class="warehouseNew__name" type="text" placeholder="instock value" />
+            <label>
+              STATUS{' '}
+              <input className="warehouseNew__name" type="text" placeholder="instock value" />
             </label>
-            <label for="itemDescription">
+            <label>
               ITEM DESCRIPTION
-              <textarea class="warehouseNew__comment" type="text" placeholder="(Optional)" />
+              <textarea className="warehouseNew__comment" type="text" placeholder="(Optional)" />
             </label>
           </form>
-          <div class="warehouseNew__buttons">
-            <button class="warehouseNew__cancel">CANCEL</button>
-            <button class="warehouseNew__button" id="warehouseNew__addButton" type="submit">
+          <div className="warehouseNew__buttons">
+            <button className="warehouseNew__cancel">CANCEL</button>
+            <button className="warehouseNew__button" id="warehouseNew__addButton" type="submit">
               SAVE
             </button>
           </div>
