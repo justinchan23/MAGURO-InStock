457e048f06e82223f674af58870241e4edf65aab
justinchan23 <justinchan23@gmail.com>
2019-03-04 18:39:17 -0800

    update packages, server basics

--- back-end/server.js
+++ back-end/server.js
@@ -1,2 +1,10 @@
 const express = require('express')
-const app = expresss()
+const app = express()
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const cors = require('cors')
+const port = process.env.PORT || process.argv[2] || 8080
+
+app.use(bodyParser.json())
+app.use(cors())
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
