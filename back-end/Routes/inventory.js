ec2120d49d7537ffece8db5bcf3884f04ea818e9
justinchan23 <justinchan23@gmail.com>
2019-03-06 09:54:58 -0800

    added route to get inventory item

--- back-end/Routes/inventory.js
+++ back-end/Routes/inventory.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const fs = require('fs')
 const data = require('../Database/inventory')
 
-router.get('/')
+router.get('/:id', (req, res) => {
+  const inventoryItem = data.find(item => {
+    return item.id == req.params.id
+  })
+  inventoryItem ? res.send(inventoryItem) : res.status(404).send('Item not found')
+})
 
 module.exports = router
