73dc294fba02e9974e8756411c53f726558248de
justinchan23 <justinchan23@gmail.com>
2019-03-06 15:32:04 -0800

    button added and functinoal

--- front-end/src/Components/InventoryNewItem.js
+++ front-end/src/Components/InventoryNewItem.js
@@ -1,110 +1,148 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../Styles/InventoryNewItem/InventoryNew.css'
 import Switch from 'react-switch'
+// import axios from 'axios'
 
 const InventoryNewItem = () => {
-  const productName = useRef()
-  const lastOrdered = useRef()
-  const city = useRef()
-  const country = useRef()
-  const quantity = useRef()
-  const description = useRef()
+  // const productName = useRef()
+  // const lastOrdered = useRef()
+  // const city = useRef()
+  // const country = useRef()
+  // const quantity = useRef()
+  // const description = useRef()
   const [stockStatus, setStockStatus] = useState(true)
+  const newItemForm = useRef()
+  const [addStatus, setAddStatus] = useState(false)
 
-  const stockChange = () => {
+  const stockStatusChange = () => {
     stockStatus ? setStockStatus(false) : setStockStatus(true)
   }
 
-  return (
-    <content className="inventoryNew">
-      <div className="inventoryNew__main">
-        <div className="inventoryNew__section">
-          <h1>Create New</h1>
-          <form className="inventoryNew__form">
-            <label>
-              PRODUCT
-              <input
-                className="inventoryNew__name"
-                type="text"
-                placeholder="Item Name"
-                required
-                ref={productName}
-              />
-            </label>
-            <label>
-              LAST ORDERED
-              <input
-                className="inventoryNew__name"
-                type="text"
-                placeholder="yyyy-mm-dd"
-                required
-                ref={lastOrdered}
-              />
-            </label>
-            <label>
-              CITY{' '}
-              <input
-                className="inventoryNew__name"
-                type="text"
-                placeholder="City"
-                required
-                ref={city}
-              />
-            </label>
-            <label>
-              COUNTRY
-              <select name="country" className="inventoryNew__select" required ref={country}>
-                <option value="" />
-                <option value="Canada">Canada</option>
-                <option value="US">US</option>
-                <option value="Mexico">Mexico</option>
-              </select>
-            </label>
-            <label>
-              QUANTITY{' '}
-              <input
-                className="inventoryNew__name"
-                type="text"
-                placeholder="0"
-                required
-                ref={quantity}
-              />
-            </label>
-            <label className="inventoryNew__switchContainer">
-              STATUS
-              <div className="inventoryNew__stock">
-                <span>IN STOCK</span>
-                <Switch
-                  checked={stockStatus}
-                  onChange={stockChange}
-                  checkedIcon={false}
-                  uncheckedIcon={false}
-                  className="inventoryNew__switch"
-                  width={46}
+  const addItemToggle = () => {
+    setAddStatus(true)
+  }
+
+  const addNewItem = () => {
+    console.log(newItemForm.current.country.value)
+    alert('Item added successfully')
+    setAddStatus(false)
+    newItemForm.current.reset()
+  }
+
+  const cancelAdd = () => {
+    newItemForm.current.reset()
+    setAddStatus(false)
+  }
+
+  useEffect(() => {}, [addStatus])
+
+  if (addStatus) {
+    return (
+      <content className="inventoryNew">
+        <div className="inventoryNew__main">
+          <div className="inventoryNew__section">
+            <h1>Create New</h1>
+            <form className="inventoryNew__form" ref={newItemForm}>
+              <label>
+                PRODUCT
+                <input
+                  className="inventoryNew__name"
+                  type="text"
+                  placeholder="Item Name"
+                  required
+                  name="productName"
                 />
-              </div>
-            </label>
-            <label>
-              ITEM DESCRIPTION
-              <textarea
-                className="inventoryNew__comment"
-                type="text"
-                placeholder="(Optional)"
-                required
-                ref={description}
-              />
-            </label>
-          </form>
-          <div className="inventoryNew__buttons">
-            <button className="inventoryNew__cancel">CANCEL</button>
-            <button className="inventoryNew__button" id="inventoryNew__addButton" type="submit">
-              SAVE
-            </button>
+              </label>
+              <label>
+                LAST ORDERED
+                <input
+                  className="inventoryNew__name"
+                  type="text"
+                  placeholder="yyyy-mm-dd"
+                  required
+                  name="lastOrdered"
+                />
+              </label>
+              <label>
+                CITY{' '}
+                <input
+                  className="inventoryNew__name"
+                  type="text"
+                  placeholder="City"
+                  required
+                  name="city"
+                />
+              </label>
+              <label>
+                COUNTRY
+                <select name="country" className="inventoryNew__select" required>
+                  <option value="" />
+                  <option value="Canada">Canada</option>
+                  <option value="US">US</option>
+                  <option value="Mexico">Mexico</option>
+                </select>
+              </label>
+              <label>
+                QUANTITY{' '}
+                <input
+                  className="inventoryNew__name"
+                  type="text"
+                  placeholder="0"
+                  required
+                  name="quantity"
+                />
+              </label>
+              <label className="inventoryNew__switchContainer">
+                STATUS
+                <div className="inventoryNew__stock">
+                  <span>IN STOCK</span>
+                  <Switch
+                    checked={stockStatus}
+                    onChange={stockStatusChange}
+                    checkedIcon={false}
+                    uncheckedIcon={false}
+                    className="inventoryNew__switch"
+                    width={46}
+                  />
+                </div>
+              </label>
+              <label>
+                ITEM DESCRIPTION
+                <textarea
+                  className="inventoryNew__comment"
+                  type="text"
+                  placeholder="(Optional)"
+                  required
+                  name="description"
+                />
+              </label>
+            </form>
+            <div className="inventoryNew__buttons">
+              <button className="inventoryNew__cancel" onClick={cancelAdd}>
+                CANCEL
+              </button>
+              <button
+                className="inventoryNew__button"
+                id="inventoryNew__addButton"
+                type="submit"
+                onClick={addNewItem}
+              >
+                SAVE
+              </button>
+            </div>
           </div>
         </div>
+      </content>
+    )
+  } else {
+    return (
+      <div>
+        <button className="inventoryNew__addButton" onClick={addItemToggle}>
+          <img src="./Icons/SVG/Icon-add.svg" alt="add" />
+        </button>
       </div>
-    </content>
-  )
+    )
+  }
 }
 
 export default InventoryNewItem
