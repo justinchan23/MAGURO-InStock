4c3f0dfa37ad6b9332085878949ccf94d0df41ce
justinchan23 <justinchan23@gmail.com>
2019-03-06 15:41:37 -0800

    button function added, minor styling change

--- front-end/src/Components/WarehouseNewLocation.js
+++ front-end/src/Components/WarehouseNewLocation.js
@@ -1,92 +1,123 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../Styles/WarehouseNew/WarehouseNew.css'
 // import Switch from 'react-switch'
 
 const WarehouseNewLocation = () => {
-  return (
-    <content className="warehouseNew">
-      <div className="warehouseNew__main">
-        <div className="warehouseNew__section">
-          <h1>Add New</h1>
-          <form className="warehouseNew__form">
-            <label className="warehouseNew__top">
-              WAREHOUSE
-              <input
-                className="warehouseNew__name"
-                type="text"
-                placeholder="Name &amp; ID"
-                required
-              />
-            </label>
-            <label>
-              ADDRESS
-              <input
-                className="warehouseNew__name"
-                type="text"
-                placeholder="Enter Address"
-                required
-              />
-            </label>
-            <label>
-              LOCATION
-              <select name="country" className="warehouseNew__select" required>
-                <option value="" />
-                <option value="Toronto, ON">Toronto, CAN</option>
-                <option value="America, US">America, US</option>
-                <option value="Mexico, MX">Mexico, MX</option>
-              </select>
-            </label>
-            <label>
-              CONTACT NAME
-              <input className="warehouseNew__name" type="text" placeholder="Enter name" required />
-            </label>
-            <label>
-              POSITION{' '}
-              <input
-                className="warehouseNew__name"
-                type="text"
-                placeholder="Enter position"
-                required
-              />
-            </label>
-            <label>
-              PHONE NUMBER{' '}
-              <input
-                className="warehouseNew__name"
-                type="text"
-                placeholder="000-000-0000"
-                required
-              />
-            </label>
-            <label>
-              EMAIL{' '}
-              <input
-                className="warehouseNew__name"
-                type="text"
-                placeholder="email@instock.com"
-                required
-              />
-            </label>
-            <label>
-              CATEGORIES
-              <textarea
-                className="warehouseNew__comment"
-                type="text"
-                placeholder="Use commas to separate each category"
-                required
-              />
-            </label>
-          </form>
-          <div className="warehouseNew__buttons">
-            <button className="warehouseNew__cancel">CANCEL</button>
-            <button className="warehouseNew__button" id="warehouseNew__addButton" type="submit">
-              SAVE
-            </button>
+  const [addWarehouseStatus, setAddWarehouseStatus] = useState(false)
+
+  const cancelAdd = () => {
+    // newItemForm.current.reset()
+    setAddWarehouseStatus(false)
+  }
+
+  const addWarehouseToggle = () => {
+    setAddWarehouseStatus(true)
+    console.log('test')
+  }
+
+  useEffect(() => {}, [addWarehouseStatus])
+
+  if (addWarehouseStatus) {
+    return (
+      <content className="warehouseNew">
+        <div className="warehouseNew__main">
+          <div className="warehouseNew__section">
+            <h1>Add New</h1>
+            <form className="warehouseNew__form">
+              <label className="warehouseNew__top">
+                WAREHOUSE
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="Name &amp; ID"
+                  required
+                />
+              </label>
+              <label>
+                ADDRESS
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="Enter Address"
+                  required
+                />
+              </label>
+              <label>
+                LOCATION
+                <select name="country" className="warehouseNew__select" required>
+                  <option value="" />
+                  <option value="Toronto, ON">Toronto, CAN</option>
+                  <option value="America, US">America, US</option>
+                  <option value="Mexico, MX">Mexico, MX</option>
+                </select>
+              </label>
+              <label>
+                CONTACT NAME
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="Enter name"
+                  required
+                />
+              </label>
+              <label>
+                POSITION{' '}
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="Enter position"
+                  required
+                />
+              </label>
+              <label>
+                PHONE NUMBER{' '}
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="000-000-0000"
+                  required
+                />
+              </label>
+              <label>
+                EMAIL{' '}
+                <input
+                  className="warehouseNew__name"
+                  type="text"
+                  placeholder="email@instock.com"
+                  required
+                />
+              </label>
+              <label>
+                CATEGORIES
+                <textarea
+                  className="warehouseNew__comment"
+                  type="text"
+                  placeholder="Use commas to separate each category"
+                  required
+                />
+              </label>
+            </form>
+            <div className="warehouseNew__buttons">
+              <button className="warehouseNew__cancel" onClick={cancelAdd}>
+                CANCEL
+              </button>
+              <button className="warehouseNew__button" id="warehouseNew__addButton" type="submit">
+                SAVE
+              </button>
+            </div>
           </div>
         </div>
+      </content>
+    )
+  } else {
+    return (
+      <div>
+        <button className="warehouseNew__addButton" onClick={addWarehouseToggle}>
+          <img src="./Icons/SVG/Icon-add.svg" alt="add" />
+        </button>
       </div>
-    </content>
-  )
+    )
+  }
 }
 
 export default WarehouseNewLocation
