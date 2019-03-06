fed4f51998645c3fcb47fb239580d1a4a6fa0bb8
Patrick Isaac <patrick.isaac04@gmail.com>
2019-03-06 13:33:23 -0800

    added mock data

--- front-end/src/Components/WarehouseLocations.js
+++ front-end/src/Components/WarehouseLocations.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import '../Styles/WarehouseLocations-CSS/styles.css'
 
+const data = {
+  warehouseName: "Warehouse Number 1",
+  warehouseAddress: "469 King St W, Toronto, ON",
+  contactName: "Mara Weinberg",
+  contactPosition: "Warehouse Manager",
+  contactNumber: "+1 416 678 2345",
+  contactEmail: "weinberg@instack.com",
+  categories: "Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation"
+}
+
+
 const WarehouseLocations = () => {
   return (
   <div className="locations__container">
@@ -18,32 +29,35 @@ const WarehouseLocations = () => {
     <table>
       <tbody>
 
+
       <tr className="locations__row">
         <th className="locations__title-desktop">WAREHOUSE</th>
-        <td className="warehouse__title">Warehouse Number 1</td>
-        <td>469 King St W, Toronto, ON</td>
+        <td className="warehouse__title">{data.warehouseName}</td>
+        <td>{data.warehouseAddress}</td>
         <th className="locations__title-desktop">CONTACT</th>
-        <td className="padding__info">Mara Weinberg</td>
-        <td className="warehouse__position">Warehouse Manager</td>
+        <td className="padding__info">{data.contactName}</td>
+        <td className="warehouse__position">{data.contactPosition}</td>
         <th className="locations__title-desktop">CONTACT INFORMATION</th>
-        <td className="padding__info">+1 416 678 2345</td>
-        <td>weinberg@instack.com</td>
+        <td className="padding__info">{data.contactNumber}</td>
+        <td>{data.contactEmail}</td>
         <th className="locations__title-desktop">CATEGORIES</th>
-        <td className="warehouse__category padding__info">Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation</td>
+        <td className="warehouse__category padding__info">{data.categories}</td>
+        <td className="img__arrow"><img src="../Icons/SVG/Icon-arrow-right.svg" alt="right-arrow"/></td>
       </tr>
 
       <tr className="locations__row">
         <th className="locations__title-desktop">WAREHOUSE</th>
-        <td className="warehouse__title">Warehouse Number 1</td>
-        <td>469 King St W, Toronto, ON</td>
+        <td className="warehouse__title">{data.warehouseName}</td>
+        <td>{data.warehouseAddress}</td>
         <th className="locations__title-desktop">CONTACT</th>
-        <td className="padding__info">Mara Weinberg</td>
-        <td className="warehouse__position">Warehouse Manager</td>
+        <td className="padding__info">{data.contactName}</td>
+        <td className="warehouse__position">{data.contactPosition}</td>
         <th className="locations__title-desktop">CONTACT INFORMATION</th>
-        <td className="padding__info">+1 416 678 2345</td>
-        <td>weinberg@instack.com</td>
+        <td className="padding__info">{data.contactNumber}</td>
+        <td>{data.contactEmail}</td>
         <th className="locations__title-desktop">CATEGORIES</th>
-        <td className="warehouse__category padding__info">Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation</td>
+        <td className="warehouse__category padding__info">{data.categories}</td>
+        <td className="img__arrow"><img src="../Icons/SVG/Icon-arrow-right.svg" alt="right-arrow"/></td>
       </tr>
 
 
