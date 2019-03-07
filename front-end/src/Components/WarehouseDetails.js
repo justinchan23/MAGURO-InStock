f0b59d8843df4a38dab3262e6877bc360b636ecf
Connor Straathof <cstraathof@gmail.com>
2019-03-06 16:31:29 -0800

    warehouse details complete

--- front-end/src/Components/WarehouseDetails.js
+++ front-end/src/Components/WarehouseDetails.js
@@ -1,67 +1,41 @@
 import React, { Component } from 'react'
-import '../Styles/InventoryItemDetail/InventoryItemDetail.css'
+import '../Styles/WarehouseDetails/WarehouseDetails.css'
 import BackButton from '../Assets/Icons/SVG/Icon-back-arrow.svg'
 
 class WarehouseDetail extends Component {
   render() {
     return (
-      <div className='iid'>
-        <div className='iid__one'>
-          <img className='iid__backbutton' src={BackButton} alt='backbutton' />
-          <h2 className='iid__one__title'>Product Name</h2>
-          <button className='iid__one__button'>In Stock</button>
+      <div className='cd'>
+        <div className='cd__one'>
+          <img className='cd__backbutton' src={BackButton} alt='backbutton' />
+          <h1 className='cd__one__title'>Warehouse Name</h1>
         </div>
-        <hr className='iid__hr' />
-        <div className='iid__desk'>
-          <div className='iid__two'>
-            <h4 className='iid__header'>ITEM DESCRIPTION</h4>
-            <p className='iid__info__two'>
-              Here is a more detailed summary of the product name, it’s uses, industries and
-              possible attributes that could be used to describe the product.
-            </p>
-          </div>
-          <div>
-            <div className='iid__info__chunk'>
-              <div className='iid__block'>
-                <div className='iid__sub'>
-                  <h4 className='iid__header'>ORDERED BY</h4>
-                  <p className='iid__info'>5/24/2018</p>
-                </div>
-                <div>
-                  <h4 className='iid__header'>LAST ORDERED</h4>
-                  <p className='iid__info'>12000</p>
-                </div>
+        <hr className='cd__hr' />
+        <div className='cd__desk'>
+          <div className='cd__info__chunk'>
+            <div className='cd__block'>
+              <h4 className='cd__header'>ADDRESS</h4>
+              <div className='cd__info__block'>
+                <p className='cd__info'>123 Main Street W.</p>
+                <p className='cd__info'>Suite 201</p>
               </div>
-              <div className='iid__block'>
-                <div className='iid__sub'>
-                  <h4 className='iid__header'>REFERENCE NUMBER</h4>
-                  <p className='iid__info'>JK2020FD7811201</p>
-                </div>
-                <div>
-                  <h4 className='iid__header'>LOCATION</h4>
-                  <p className='iid__info'>Toronto, CAN</p>
-                </div>
+              <div className='cd__info__block'>
+                <p className='cd__info'>Toronto,</p>
+                <p className='cd__info'>ON M65GB7 CA</p>
               </div>
             </div>
-            <div className='iid__block__bottom'>
-              <div className='iid__sub'>
-              <div className='iid__block'>
-                <div className='iid__sub'>
-                  <h4 className='iid__header'>QUANTITY</h4>
-                  <p className='iid__info'>12000</p>
-                </div>
+            <div className='cd__block'>
+              <h4 className='cd__header'>CONTACT</h4>
+              <div className='cd__info__block'>
+                <p className='cd__info'>Mara Weinberg</p>
+                <p className='cd__info'>Warehouse Manager</p>
               </div>
-                <h4 className='iid__header'>CATEGORIES</h4>
-                <p className='iid__info'>
-                  Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation, Sales
-                </p>
+              <div className='cd__info__block'>
+                <p className='cd__info'>+1 416 678 2345</p>
+                <p className='cd__info'>weinberg@instock.com</p>
               </div>
             </div>
           </div>
-        </div>
-        <hr className='iid__hr' />
-        <div className='iid__block__button'>
-          <button className='iid__button'>EDIT</button>
         </div>
       </div>
     )
