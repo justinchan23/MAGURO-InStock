a5f88d9a32a44ceed3490813f4855e85627e0aaf
Patrick Isaac <patrick.isaac04@gmail.com>
2019-03-08 11:39:20 -0800

    fixed styling for inventory page

--- front-end/src/Components/Inventory.js
+++ front-end/src/Components/Inventory.js
@@ -8,7 +8,7 @@ const inventory = [
     lastOrd: '05/24/2018',
     location: 'Toronto, Canada',
     quantity: '12,000',
-    inStock: "In Stock"
+    inStock: 'In Stock'
   }
 ]
 
@@ -16,47 +16,48 @@ class Inventory extends React.Component {
   render() {
     const inventoryList = inventory.map((item, id) => {
       return (
-        <tr className="inventory__row" key={id}>
-        <th className="inventory__header mobile">ITEM</th>
-        <td><b>{item.itemName}</b><br />{item.itemDes}</td>
-        <th className="inventory__header mobile">LAST ORDERED</th>
-        <td>{item.lastOrd}</td>
-        <th className="inventory__header mobile">LOCATION</th>
-        <td>{item.location}</td>
-        <th className="inventory__header mobile">QUANTITY</th>
-        <td>{item.quantity}</td>
-        <th className="inventory__header mobile">STATUS</th>
-        <td>{item.inStock}</td>
-        <td className="kebab"><img src="../Icons/SVG/Icon-kebab-default.svg" alt="" /></td>
-      </tr>
+        <tr className='inventory__row' key={id}>
+          <th className='inventory__header mobile'>ITEM</th>
+          <td className='inventory__item'>
+            <b>{item.itemName}</b>
+            <br />
+            {item.itemDes}
+          </td>
+          <th className='inventory__header mobile'>LAST ORDERED</th>
+          <td>{item.lastOrd}</td>
+          <th className='inventory__header mobile'>LOCATION</th>
+          <td>{item.location}</td>
+          <th className='inventory__header mobile'>QUANTITY</th>
+          <td className='inventory__quantity'>{item.quantity}</td>
+          <th className='inventory__header mobile'>STATUS</th>
+          <td>{item.inStock}</td>
+          <td className='kebab'>
+            <img src='../Icons/SVG/Icon-kebab-default.svg' alt='' />
+          </td>
+        </tr>
       )
     })
     return (
-      <div className="inventory__container">
-        <div className="inventory__container-header">
+      <div className='inventory__container'>
+        <div className='inventory__container-header'>
           <h1>Inventory</h1>
-          <input
-            type="text"
-            placeholder="Search"
-            className="inventory__search"
-            alt="search"
-          />
+          <input type='text' placeholder='Search' className='inventory__search' alt='search' />
         </div>
-        <div className="inventory__table">
+        <div className='inventory__table'>
           <table>
             <tbody>
-              <tr className="inventory__header--desktop">
-                <th className="inventory__header desktop">ITEM</th>
-                <th className="inventory__header desktop">LAST ORDERED</th>
-                <th className="inventory__header desktop">LOCATION</th>
-                <th className="inventory__header desktop">QUANTITY</th>
-                <th className="inventory__header desktop">STATUS</th>
+              <tr className='inventory__header--desktop'>
+                <th className='inventory__header inventory__item desktop'>ITEM</th>
+                <th className='inventory__header desktop'>LAST ORDERED</th>
+                <th className='inventory__header desktop'>LOCATION</th>
+                <th className='inventory__header desktop'>QUANTITY</th>
+                <th className='inventory__header desktop'>STATUS</th>
               </tr>
               {inventoryList}
               {inventoryList}
             </tbody>
-          </table> 
-        </div> 
+          </table>
+        </div>
       </div>
     )
   }
--- front-end/src/Components/WarehouseLocations.js
+++ front-end/src/Components/WarehouseLocations.js
@@ -19,45 +19,45 @@ class WarehouseLocations extends React.Component {
   render() {
     const warehouseList = warehouses.map((warehouse, id) => {
       return (
-        <tr className="locations__row" key={id}>
-          <td className="warehouse__title">
+        <tr className='locations__row' key={id}>
+          <td className='warehouse__title'>
             <b>{warehouse.name}</b>
             <br />
             {warehouse.address}
           </td>
-          <td className="padding__info">
+          <td className='padding__info'>
             {warehouse.ContactName}
             <br />
             <i>{warehouse.ContactPosition}</i>
           </td>
-          <td className="padding__info">
+          <td className='padding__info'>
             {warehouse.ContactNumber}
             <br />
             {warehouse.ContactEmail}
           </td>
-          <td className="warehouse__category padding__info category">{warehouse.categories}</td>
-          <td className="img__arrow">
-            <Link to="/warehousedetails">
-              <img src="../Icons/SVG/Icon-arrow-right.svg" alt="right-arrow" />
+          <td className='warehouse__category padding__info category'>{warehouse.categories}</td>
+          <td className='img__arrow'>
+            <Link to='/warehousedetails'>
+              <img src='../Icons/SVG/Icon-arrow-right.svg' alt='right-arrow' />
             </Link>
           </td>
         </tr>
       )
     })
     return (
-      <div className="locations__container">
-        <div className="locations__container-header">
+      <div className='locations__container'>
+        <div className='locations__container-header'>
           <h1>Locations</h1>
-          <input type="text" placeholder="Search" className="locations__search" alt="search" />
+          <input type='text' placeholder='Search' className='locations__search' alt='search' />
         </div>
-        <div className="locations__wrapper">
+        <div className='locations__wrapper'>
           <table>
             <tbody>
-              <tr className="locations__titles">
-                <th className="locations__title-desktop warehouse">WAREHOUSE</th>
-                <th className="locations__title-desktop contact">CONTACT</th>
-                <th className="locations__title-desktop contact__info">CONTACT INFORMATION</th>
-                <th className="locations__title-desktop">CATEGORIES</th>
+              <tr className='locations__titles'>
+                <th className='locations__title-desktop warehouse'>WAREHOUSE</th>
+                <th className='locations__title-desktop contact'>CONTACT</th>
+                <th className='locations__title-desktop contact__info'>CONTACT INFORMATION</th>
+                <th className='locations__title-desktop'>CATEGORIES</th>
               </tr>
               {warehouseList}
               {warehouseList}
