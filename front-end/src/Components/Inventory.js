658298d3b2f1aabe55b47b64bb9770fd431aa7fa
justinchan23 <justinchan23@gmail.com>
2019-03-08 14:32:57 -0800

    added inventorynewitem component to inventory

--- front-end/src/Components/Inventory.js
+++ front-end/src/Components/Inventory.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import InventoryTable from './InventoryTable'
 import '../Styles/Inventory/styles.css'
-
+import InventoryNewItem from './InventoryNewItem'
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div className='inventory__container'>
-        <div className='inventory__container-header'>
+      <div className="inventory__container">
+        <div className="inventory__container-header">
           <h1>Inventory</h1>
-          <input type='text' placeholder='Search' className='inventory__search' alt='search' />
+          <input type="text" placeholder="Search" className="inventory__search" alt="search" />
         </div>
         <InventoryTable />
+        <InventoryNewItem />
       </div>
     )
   }
