41ad6c2a30681f7cc291f9aeaa7b13cc4787d25c
Patrick Isaac <patrick.isaac04@gmail.com>
2019-03-08 15:12:16 -0800

    added warehouse back-end / styling still needs to be fixed

--- back-end/Routes/warehouses.js
+++ back-end/Routes/warehouses.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const fs = require('fs')
 const data = require('../Database/warehouses')
 
-router.get('/')
+router.get('/', (req, res) => {
+  res.json(data)
+})
+
+
 
 module.exports = router
