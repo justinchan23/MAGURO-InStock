ceeffd0f7e8fc8c51f3e1255f125c20463afc1bf
justinchan23 <justinchan23@gmail.com>
2019-03-08 16:26:46 -0800

    update style background

--- front-end/src/Components/WarehouseLocations.js
+++ front-end/src/Components/WarehouseLocations.js
@@ -4,66 +4,65 @@ import WarehouseNewLocation from './WarehouseNewLocation'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const url = "http://localhost:8080/warehouses"
+const url = 'http://localhost:8080/warehouses'
 
 class WarehouseLocations extends React.Component {
   state = {
     warehouses: [],
-    id: ""
+    id: ''
   }
 
   componentDidMount() {
-    axios.get(url) 
-      .then(response => {
-        this.setState({
-          warehouses: response.data,
-          id: response.data.id
-        })
+    axios.get(url).then(response => {
+      this.setState({
+        warehouses: response.data,
+        id: response.data.id
       })
+    })
   }
 
   render() {
     const warehouseList = this.state.warehouses.map((warehouse, id) => {
       return (
-        <tr className='locations__row' key={id}>
-          <td className='warehouse__title'>
+        <tr className="locations__row" key={id}>
+          <td className="warehouse__title">
             <b>{warehouse.warehouse_name}</b>
             <br />
-            {warehouse.address + " " + warehouse.postal_code + " " + warehouse.country_code}
+            {warehouse.address + ' ' + warehouse.postal_code + ' ' + warehouse.country_code}
           </td>
-          <td className='padding__info'>
+          <td className="padding__info">
             {warehouse.contact_name}
             <br />
             <i>{warehouse.contact_title}</i>
           </td>
-          <td className='padding__info'>
+          <td className="padding__info">
             {warehouse.contact_phone}
             <br />
             {warehouse.contact_email}
           </td>
-          <td className='warehouse__category padding__info category'>{warehouse.categories}</td>
-          <td className='img__arrow'>
-            <Link to='/warehousedetails'>
-              <img src='../Icons/SVG/Icon-arrow-right.svg' alt='right-arrow' />
+          <td className="warehouse__category padding__info category">{warehouse.categories}</td>
+          <td className="img__arrow">
+            <Link to="/warehousedetails">
+              <img src="../Icons/SVG/Icon-arrow-right.svg" alt="right-arrow" />
             </Link>
           </td>
         </tr>
       )
     })
     return (
-      <div className='locations__container'>
-        <div className='locations__container-header'>
+      <div className="locations__container">
+        <div className="locations__container-header">
           <h1>Locations</h1>
-          <input type='text' placeholder='Search' className='locations__search' alt='search' />
+          <input type="text" placeholder="Search" className="locations__search" alt="search" />
         </div>
-        <div className='locations__wrapper'>
+        <div className="locations__wrapper">
           <table>
             <tbody>
-              <tr className='locations__titles'>
-                <th className='locations__title-desktop warehouse'>WAREHOUSE</th>
-                <th className='locations__title-desktop contact'>CONTACT</th>
-                <th className='locations__title-desktop contact__info'>CONTACT INFORMATION</th>
-                <th className='locations__title-desktop'>CATEGORIES</th>
+              <tr className="locations__titles">
+                <th className="locations__title-desktop">WAREHOUSE</th>
+                <th className="locations__title-desktop contact">CONTACT</th>
+                <th className="locations__title-desktop contact__info">CONTACT INFORMATION</th>
+                <th className="locations__title-desktop">CATEGORIES</th>
               </tr>
               {warehouseList}
               {warehouseList}
