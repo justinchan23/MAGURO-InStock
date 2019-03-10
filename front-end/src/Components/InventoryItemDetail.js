e6850eb98f5ff9045c7ae1be139c2d26241ae513
Connor Straathof <cstraathof@gmail.com>
2019-03-09 16:13:48 -0800

    back end and axios work

--- front-end/src/Components/InventoryItemDetail.js
+++ front-end/src/Components/InventoryItemDetail.js
@@ -2,69 +2,76 @@ import React, { Component } from 'react'
 import '../Styles/InventoryItemDetail/InventoryItemDetail.css'
 import BackButton from '../Assets/Icons/SVG/Icon-back-arrow.svg'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class InventoryItemDetail extends Component {
+  state = {
+    itemDetails: []
+  }
+  componentDidMount() {
+    this.getInventoryDetail()
+  }
+  getInventoryDetail = () => {
+    axios.get(`http://localhost:8080/inventory/${this.props.match.params.id}`).then(response => {
+      this.setState({itemDetails: response.data})
+    })
+  }
   render() {
     return (
-      <div className="iid">
-        <div className="iid__one">
-          <Link to="/inventory">
-            <img className="iid__backbutton" src={BackButton} alt="backbutton" />
+      <div className='iid'>
+        <div className='iid__one'>
+          <Link to='/inventory'>
+            <img className='iid__backbutton' src={BackButton} alt='backbutton' />
           </Link>
-          <h2 className="iid__one__title">Product Name</h2>
-          <button className="iid__one__button">In Stock</button>
+          <h2 className='iid__one__title'>{this.state.name}</h2>
+          <button className='iid__one__button'>In Stock</button>
         </div>
-        <hr className="iid__hr" />
-        <div className="iid__desk">
-          <div className="iid__two">
-            <h4 className="iid__header">ITEM DESCRIPTION</h4>
-            <p className="iid__info__two">
-              Here is a more detailed summary of the product name, it’s uses, industries and
-              possible attributes that could be used to describe the product.
-            </p>
+        <hr className='iid__hr' />
+        <div className='iid__desk'>
+          <div className='iid__two'>
+            <h4 className='iid__header'>ITEM DESCRIPTION</h4>
+            <p className='iid__info__two'>{this.state.short_description}</p>
           </div>
           <div>
-            <div className="iid__info__chunk">
-              <div className="iid__block">
-                <div className="iid__sub">
-                  <h4 className="iid__header">ORDERED BY</h4>
-                  <p className="iid__info">5/24/2018</p>
+            <div className='iid__info__chunk'>
+              <div className='iid__block'>
+                <div className='iid__sub'>
+                  <h4 className='iid__header'>ORDERED BY</h4>
+                  <p className='iid__info'>{this.state.itemDetails.ordered_by}</p>
                 </div>
                 <div>
-                  <h4 className="iid__header">LAST ORDERED</h4>
-                  <p className="iid__info">12000</p>
+                  <h4 className='iid__header'>LAST ORDERED</h4>
+                  <p className='iid__info'>{this.state.itemDetails.last_ordered}</p>
                 </div>
               </div>
-              <div className="iid__block">
-                <div className="iid__sub">
-                  <h4 className="iid__header">REFERENCE NUMBER</h4>
-                  <p className="iid__info">JK2020FD7811201</p>
+              <div className='iid__block'>
+                <div className='iid__sub'>
+                  <h4 className='iid__header'>REFERENCE NUMBER</h4>
+                  <p className='iid__info'>{this.state.itemDetails.reference_number}</p>
                 </div>
                 <div>
-                  <h4 className="iid__header">LOCATION</h4>
-                  <p className="iid__info">Toronto, CAN</p>
+                  <h4 className='iid__header'>LOCATION</h4>
+                  <p className='iid__info'>{this.state.itemDetails.location}</p>
                 </div>
               </div>
             </div>
-            <div className="iid__block__bottom">
-              <div className="iid__sub">
-                <div className="iid__block">
-                  <div className="iid__sub">
-                    <h4 className="iid__header">QUANTITY</h4>
-                    <p className="iid__info">12000</p>
+            <div className='iid__block__bottom'>
+              <div className='iid__sub'>
+                <div className='iid__block'>
+                  <div className='iid__sub'>
+                    <h4 className='iid__header'>QUANTITY</h4>
+                    <p className='iid__info'>{this.state.itemDetails.quantity}</p>
                   </div>
                 </div>
-                <h4 className="iid__header">CATEGORIES</h4>
-                <p className="iid__info">
-                  Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation, Sales
-                </p>
+                <h4 className='iid__header'>CATEGORIES</h4>
+                <p className='iid__info'>{this.state.itemDetails.categories}</p>
               </div>
             </div>
           </div>
         </div>
-        <hr className="iid__hr" />
-        <div className="iid__block__button">
-          <button className="iid__button">EDIT</button>
+        <hr className='iid__hr' />
+        <div className='iid__block__button'>
+          <button className='iid__button'>EDIT</button>
         </div>
       </div>
     )
