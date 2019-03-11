13100e7ba44c0a98a6f69e727595ff5baa0fc11d
justinchan23 <justinchan23@gmail.com>
2019-03-11 11:37:33 -0700

    modify routes and hover on links

--- front-end/src/Components/WarehouseDetails.js
+++ front-end/src/Components/WarehouseDetails.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../Styles/WarehouseDetails/WarehouseDetails.css'
 import '../Styles/Inventory/styles.css'
 import BackButton from '../Assets/Icons/SVG/Icon-back-arrow.svg'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import InventoryTable from './InventoryTable'
 import axios from 'axios'
 
@@ -25,44 +25,48 @@ class WarehouseDetails extends Component {
     if (this.state.dataLoaded === true) {
       return (
         <div>
-          <div className="cd__top__general">
+          <div className="cd__top__general slide-in-left">
             <div className="cd">
               <div className="cd__one">
-                <Link to="/warehouses">
-                  <img className="cd__backbutton" src={BackButton} alt="backbutton" />
-                </Link>
-                <h1 className='cd__one__title'>{this.state.currentdata.name}</h1>
-          </div>
-          <hr className='cd__hr' />
-          <div className='cd__desk'>
-            <div className='cd__info__chunk'>
-              <div className='cd__block'>
-                <h4 className='cd__header'>ADDRESS</h4>
-                <div className='cd__info__block'>
-                  <p className='cd__info'>{this.state.currentdata.address}</p>
-                  <p className='cd__info'>Suite 201</p>
-                </div>
-                <div className='cd__info__block'>
-                  <p className='cd__info'>{}</p>
-                  <p className='cd__info'>{this.state.currentdata.postal_code}</p>
-                </div>
+                <img
+                  className="cd__backbutton"
+                  src={BackButton}
+                  alt="backbutton"
+                  onClick={() => this.props.history.goBack()}
+                />
+
+                <h1 className="cd__one__title">{this.state.currentdata.name}</h1>
               </div>
-              <div className='cd__block'>
-                <h4 className='cd__header'>CONTACT</h4>
-                <div className='cd__info__block'>
-                  <p className='cd__info'>{this.state.currentdata.contact_name}</p>
-                  <p className='cd__info'>{this.state.currentdata.contact_title}</p>
-                </div>
-                <div className='cd__info__block'>
-                  <p className='cd__info'>{this.state.currentdata.contact_phone}</p>
-                  <p className='cd__info'>{this.state.currentdata.contact_email}</p>
+              <hr className="cd__hr" />
+              <div className="cd__desk">
+                <div className="cd__info__chunk">
+                  <div className="cd__block">
+                    <h4 className="cd__header">ADDRESS</h4>
+                    <div className="cd__info__block">
+                      <p className="cd__info">{this.state.currentdata.address}</p>
+                      <p className="cd__info">Suite 201</p>
+                    </div>
+                    <div className="cd__info__block">
+                      <p className="cd__info">{}</p>
+                      <p className="cd__info">{this.state.currentdata.postal_code}</p>
+                    </div>
+                  </div>
+                  <div className="cd__block">
+                    <h4 className="cd__header">CONTACT</h4>
+                    <div className="cd__info__block">
+                      <p className="cd__info">{this.state.currentdata.contact_name}</p>
+                      <p className="cd__info">{this.state.currentdata.contact_title}</p>
+                    </div>
+                    <div className="cd__info__block">
+                      <p className="cd__info">{this.state.currentdata.contact_phone}</p>
+                      <p className="cd__info">{this.state.currentdata.contact_email}</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="inventory__container warehouse">
+          <div className="inventory__container warehouse ">
             <InventoryTable inventory={this.state.currentdata.inventory} />
           </div>
         </div>
