ef9b973d51daa7c39a1538a9494eaaed1e029208
justinchan23 <justinchan23@gmail.com>
2019-03-12 12:19:48 -0700

    added comma separator to quantity

--- front-end/src/Components/InventoryRow.js
+++ front-end/src/Components/InventoryRow.js
@@ -3,29 +3,36 @@ import '../Styles/Inventory/styles.css'
 import { Link } from 'react-router-dom'
 import InventoryRmvBtn from './InventoryRmvBtn'
 
-const InventoryRow = ({ itemName, itemDes, lastOrd, location, quantity, inStock, id, getInventory, removeItem }) => (
-  <tr className='inventory__row'>
-    <th className='inventory__header mobile'>ITEM</th>
-    <td className='inventory__item'>
+const InventoryRow = ({
+  itemName,
+  itemDes,
+  lastOrd,
+  location,
+  quantity,
+  inStock,
+  id,
+  getInventory,
+  removeItem
+}) => (
+  <tr className="inventory__row">
+    <th className="inventory__header mobile">ITEM</th>
+    <td className="inventory__item">
       <Link to={`/inventory/` + id}>
         <b>{itemName}</b>
       </Link>
       <br />
       {itemDes}
     </td>
-    <th className='inventory__header mobile'>LAST ORDERED</th>
+    <th className="inventory__header mobile">LAST ORDERED</th>
     <td>{lastOrd}</td>
-    <th className='inventory__header mobile'>LOCATION</th>
+    <th className="inventory__header mobile">LOCATION</th>
     <td>{location}</td>
-    <th className='inventory__header mobile'>QUANTITY</th>
-    <td className='inventory__quantity'>{quantity}</td>
-    <th className='inventory__header mobile'>STATUS</th>
+    <th className="inventory__header mobile">QUANTITY</th>
+    <td className="inventory__quantity">{quantity.toLocaleString()}</td>
+    <th className="inventory__header mobile">STATUS</th>
     <td>{inStock}</td>
-    <td className='kebab'>
-      <InventoryRmvBtn
-        id={id}
-        getInventory={getInventory}
-        removeItem={removeItem} />
+    <td className="kebab">
+      <InventoryRmvBtn id={id} getInventory={getInventory} removeItem={removeItem} />
     </td>
   </tr>
 )
