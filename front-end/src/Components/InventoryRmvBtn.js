6fc7f28d6d1a1d6fff4c3243d4ccadb05682a422
Patrick Isaac <patrick.isaac04@gmail.com>
2019-03-11 17:22:05 -0700

    remove button on kebab has been added

--- front-end/src/Components/InventoryRmvBtn.js
+++ front-end/src/Components/InventoryRmvBtn.js
@@ -2,28 +2,46 @@ import React from 'react'
 import '../Styles/InventoryRmvBtn/styles.css'
 
 class InventoryRmvBtn extends React.Component {
-  state = {
-    showBtn: false
+  constructor() {
+    super();
+
+    this.toggleBtn = this.toggleBtn.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+
+    this.state = {
+      showBtn: false
+    };
   }
 
   toggleBtn() {
-    this.setState({
-      showBtn: !this.state.showBtn
-    })
+    if (!this.state.showBtn) {
+      // attach/remove event handler
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+
+    this.setState(prevState => ({
+      showBtn: !prevState.showBtn,
+    }));
+  }
+
+  handleOutsideClick(event) {
+    if (this.node.contains(event.target)) {
+      return;
+    }
+
+    this.toggleBtn();
   }
 
   render() {
     return (
-      <div>
-        {
-          this.state.showBtn?
-          <div>
-            Remove
-          </div>
-        :null  
-        }
-
-        <button onClick={()=>this.toggleBtn()}>Click Me</button>
+      <div className="toggleContainer" ref={node => { this.node = node }}>
+        <button onClick={this.toggleBtn} className="toggleBtn">
+          <img src="../Icons/SVG/Icon-kebab-default.svg" alt="remove" />
+        </button>
+        {this.state.showBtn && (<div><button className="removeBtn">Remove
+        </button></div>)}
       </div>
     )
   }
--- front-end/src/Components/InventoryRow.js
+++ front-end/src/Components/InventoryRow.js
@@ -3,30 +3,26 @@ import '../Styles/Inventory/styles.css'
 import { Link } from 'react-router-dom'
 import InventoryRmvBtn from './InventoryRmvBtn'
 
-
-
 const InventoryRow = ({ itemName, itemDes, lastOrd, location, quantity, inStock, id }) => (
-
-  <tr className="inventory__row">
-    <th className="inventory__header mobile">ITEM</th>
-    <td className="inventory__item">
+  <tr className='inventory__row'>
+    <th className='inventory__header mobile'>ITEM</th>
+    <td className='inventory__item'>
       <Link to={`/inventory/` + id}>
         <b>{itemName}</b>
       </Link>
       <br />
       {itemDes}
     </td>
-    <th className="inventory__header mobile">LAST ORDERED</th>
+    <th className='inventory__header mobile'>LAST ORDERED</th>
     <td>{lastOrd}</td>
-    <th className="inventory__header mobile">LOCATION</th>
+    <th className='inventory__header mobile'>LOCATION</th>
     <td>{location}</td>
-    <th className="inventory__header mobile">QUANTITY</th>
-    <td className="inventory__quantity">{quantity}</td>
-    <th className="inventory__header mobile">STATUS</th>
+    <th className='inventory__header mobile'>QUANTITY</th>
+    <td className='inventory__quantity'>{quantity}</td>
+    <th className='inventory__header mobile'>STATUS</th>
     <td>{inStock}</td>
-    <td className="kebab">
+    <td className='kebab'>
       <InventoryRmvBtn />
-      {/* <img src="../Icons/SVG/Icon-kebab-default.svg" alt="remove" /> */}
     </td>
   </tr>
 )
